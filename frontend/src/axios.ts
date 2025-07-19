9ad64f53fceea104fae7d84a6a45ffe1c8b9d06d
cboiam <cristianboiam@hotmail.com>
2025-07-19 22:16:05 +0100

    Expose ports and api host through env

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import qs from "qs";
 
 const service = axios.create({
-    baseURL: "http://localhost:5000/v1",
+    baseURL: `${process.env.API}/v1`,
     paramsSerializer: function (params) {
         return qs.stringify(params, { arrayFormat: "comma", encodeValuesOnly: true })
     },
